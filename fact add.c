f2400e28f74901a900c7384324257fa081a11967
priyankakhamkar16 <118741221+priyankakhamkar16@users.noreply.github.com>
2022-11-21 19:30:28 +0530

    fact add.c

---
+++ fact add.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void add();
+main()
+{
+  int i,fact=1,n;
+  printf("\n enter n= ");
+  scanf("%d", &n);
+  for(i=1; i<n; i++)
+  {
+    fact=fact*i;
+  }
+  printf("\n fact=%d", fact);
+  add();
+}
+void add()
+{
+  int a,b,c;
+  printf("\n enter the two no= ");
+  scanf("%d%d", &a,&b);
+  c=a+b;
+  printf("\n addition of two no",c);
+}
+  
