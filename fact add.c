9cd4c2e7ea483fa36bf5e8c5b0889f2e9698c489
priyankakhamkar16 <118741221+priyankakhamkar16@users.noreply.github.com>
2022-11-22 11:47:14 +0530

    Revert "sub() function is added"

--- fact add.c
+++ fact add.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 void add();
-void sub();
 main()
 {
   int i,fact=1,n;
@@ -20,13 +19,5 @@ void add()
   scanf("%d%d", &a,&b);
   c=a+b;
   printf("\n addition of two no",c);
-}
-  void sub()
-{
-  int a,b,c;
-  printf("\n enter the two no= ");
-  scanf("%d%d", &a,&b);
-  c=a-b;
-  printf("\n substraction of two no",c);
 }
   
