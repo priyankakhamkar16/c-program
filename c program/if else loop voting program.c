772a2c8c6eaf8c1c3ae5748b0fe6685ba05b0ea8
Priyanka khamkar <118741221+priyankakhamkar16@users.noreply.github.com>
2022-12-25 18:47:25 +0530

    Add files via upload

---
+++ c program/if else loop voting program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int age;
+    printf(" enter age= ");
+    scanf("%d", &age);
+    if(age>18)
+    {
+    	printf(" person is eligible for votting");
+    }
+    else
+    {
+    	printf(" person is not eligible for votting");
+
+	}
+	}
